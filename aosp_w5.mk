48c2a992acd862fa6ac2c8eaa1526e52419ab7e0
Nguyen Nhat Truong <30290559+vatva691@users.noreply.github.com>
2018-06-25 13:09:52 +0700

    Update aosp_w5.mk
    fix vendor error

--- aosp_w5.mk
+++ aosp_w5.mk
@@ -6,7 +6,7 @@ PRODUCT_DEVICE := w5
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on w5
 PRODUCT_MANUFACTURER := lge
-PRODUCT_RESTRICT_VENDOR_FILES := flase
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 $(call inherit-product, device/lge/w5/device_w5.mk)
 $(call inherit-product-if-exists, vendor/lge/w5/w5-vendor.mk)
